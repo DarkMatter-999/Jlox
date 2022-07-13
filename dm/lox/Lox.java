5e4f2fdc9efc51801c7b29c334cbe76bf03a5750
DarkMatter-999 <darkmatter999official@gmail.com>
2022-07-13 19:03:48 +0530

    Added Basic Interpreter

--- dm/lox/Lox.java
+++ dm/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String [] args) throws IOException{
         if(args.length > 1) {
@@ -27,6 +29,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);        
     }
 
     private static void runPrompt() throws IOException {
@@ -53,11 +56,13 @@ public class Lox {
 
         if(hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        // System.out.println(new AstPrinter().print(expression));
         
         // for (Token token : tokens) {
         //     System.out.println(token);
         // }
+
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String msg) {
@@ -76,6 +81,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+    
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
     
 }
